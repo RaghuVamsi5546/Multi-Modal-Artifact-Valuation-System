7e392b14cfba2bca37ec04749ca63c5ff12cf950
RaghuVamsi5546 <raghuvamsibolem@gmail.com>
2025-07-17 15:36:45 +0530

    Updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 from src.utils.common import *
 
@@ -11,5 +12,8 @@ class DataIngestion:
     def read_data(self):
         try:
             df=pd.read_csv(self.config.local_file)
+            os.makedirs(self.config.root_dir, exist_ok=True)
+            df.to_csv(os.path.join(self.config.root_dir, 'raw.csv'), index=False)
+            return df
         except Exception as e:
             raise e
--- src/components/data_validation.py
+++ src/components/data_validation.py
@@ -1,15 +1,16 @@
 import pandas as pd
-
+import os
 from src.utils.common import *
 
 from src.entity import DataValidationConfig
+from sklearn.model_selection import train_test_split
 
 class DataValidation:
     def __init__(self,config:DataValidationConfig,schema):
         self.config=config
         self.schema_path=schema
         
-    def validate_columns(self)->bool:
+    def validate_columns(self):
         try:
             status=True
             messages=[]
@@ -40,6 +41,23 @@ class DataValidation:
                         print(actual_dtype,col_type)    
             with open(self.config.validation_status_path, 'w') as f:
                 f.write(str(status))
-            return status
+    
+            if status == True:
+                X=df.drop(columns=['preservation_score'])
+                y=df['preservation_score']
+                X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+                X_train,X_valid,y_train,y_valid=train_test_split(X_train,y_train,test_size=0.2,random_state=42)
+
+                train_df = pd.concat([X_train, y_train], axis=1)
+                valid_df = pd.concat([X_valid, y_valid], axis=1)
+                test_df = pd.concat([X_test, y_test], axis=1)
+
+                os.makedirs(self.config.root_dir, exist_ok=True)
+                train_df.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)
+                valid_df.to_csv(os.path.join(self.config.root_dir, "validation.csv"), index=False)
+                test_df.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)
+                
+            return status,train_df,valid_df,test_df
+
         except Exception as e:
             raise e
